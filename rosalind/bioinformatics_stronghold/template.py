0866c43c1a957ba3722377acfdae121bd7e650e3
piero <piero.anic@gmail.com>
2025-01-16 12:14:48 +0100

    + restructure

---
+++ rosalind/bioinformatics_stronghold/template.py
@@ -0,0 +1,44 @@
+from collections import defaultdict as DD
+
+def ints(arr):
+    return list(map(int, arr.split(' ')))
+
+def strs(arr):
+    return list(map(str, arr))
+
+def from_data(data, delim='\n'):
+    return data.split(delim)
+
+def to_graph_weighted(data, undirected = True):
+    [dd, *E] = from_data(data)
+    [v, e] = ints(dd)
+
+    G = DD(list)
+    for r in E:
+        [a, b, w] = ints(r)
+        G[a].append((b, w))
+        if undirected:
+            G[b].append((a, w))
+    return [v, e, G]
+def to_graph(data, undirected = True):
+    [dd, *E] = from_data(data)
+    [v, e] = ints(dd)
+
+    G = DD(list)
+    for r in E:
+        [a, b] = ints(r)
+        G[a].append(b)
+        if undirected:
+            G[b].append(a)
+    return [v, e, G]
+
+def inline(*data):
+    print(*data, end=" ")
+
+def disp(data):
+    print(" ".join(strs(data)))
+
+with open('') as __f:
+    data = __f.read().strip()
+
+
