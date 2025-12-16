ed4429cb8530526f52f3dc4c5e6a209c6aa013b6
piero <piero.anic@gmail.com>
2025-12-16 15:53:35 +0100

    + helpers

--- everybody_codes/make_new.sh
+++ everybody_codes/make_new.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
+### Usage:
+### ./make_new <day> <year>
+### day is formatted by 2 digits (e.g. 07 or 23)
+### Example ./make_new 01 2025
+
 cp template.js "$1.js";
 echo "main('$1');" >> "$1.js";
 echo "" >> "$1".js;
 
 touch "$1"{a,b,c}_input;
-subl "$1"*;
+mv "$1"* "$2"*
+subl "$2"*/"$1"*;
--- everybody_codes/template.js
+++ everybody_codes/template.js
@@ -1,19 +1,19 @@
 
 const fs = require('fs');
 const { exec } = require("child_process");
-const { ALPHA_L, ALPHA_U, NUMS, D4, D6, D8, MOD } = require("../../aoc/lib");
-const { joseph, findCycle } = require("../../aoc/lib");
-const { cache } = require("../../aoc/lib");
-const { bin, float, hex, int, num, oct } = require("../../aoc/lib");
-const { range, drange, trange, iter, diter, titer } = require("../../aoc/lib");
-const { copy,	entries, in_, inBB, keys, sort, values } = require("../../aoc/lib");
-const { Counter, DD, empty, FastQueue, PriorityQueue, Queue, set, Stack } = require("../../aoc/lib");
-const { areaInt, circumference, manDist, shoelace } = require("../../aoc/lib");
-const { ord, chr, count, debug, disp, disp3, crt, gcd, lcm, modPow, modPowBig, modInv, mod, prod, prodBig, randint, sum, sumBig, transpose } = require("../../aoc/lib");
-const { digits, ints,	floats,	singles,	words,	lines,	table,	groups,	getGroups,	groupsWith,	parse,	parseLine, scanf } = require ('../../aoc/lib');
+const { ALPHA_L, ALPHA_U, NUMS, D4, D6, D8, MOD } = require("../../../aoc/lib");
+const { joseph, findCycle } = require("../../../aoc/lib");
+const { cache } = require("../../../aoc/lib");
+const { bin, float, hex, int, num, oct } = require("../../../aoc/lib");
+const { range, drange, trange, iter, diter, titer } = require("../../../aoc/lib");
+const { copy,	entries, in_, inBB, keys, sort, values } = require("../../../aoc/lib");
+const { Counter, DD, empty, FastQueue, PriorityQueue, Queue, set, Stack } = require("../../../aoc/lib");
+const { areaInt, circumference, manDist, shoelace } = require("../../../aoc/lib");
+const { ord, chr, count, debug, disp, disp3, crt, gcd, lcm, modPow, modPowBig, modInv, mod, prod, prodBig, randint, sum, sumBig, transpose } = require("../../../aoc/lib");
+const { digits, ints,	floats,	singles,	words,	lines,	table,	groups,	getGroups,	groupsWith,	parse,	parseLine, scanf } = require ('../../../aoc/lib');
 const { min, max, random, abs, ceil, floor, log, log10, log2, round, sign, sin, cos, tan, asin, acos, atan, atan2, sqrt, PI } = Math;
 const { disjoint, isSubset, isSuperset, or, and, xor, sub } = set;
-const { combinations, combinations_with_replacement, next_permutation, product, unique_permutations } = require("../../aoc/lib");
+const { combinations, combinations_with_replacement, next_permutation, product, unique_permutations } = require("../../../aoc/lib");
 
 function part1(data) {
 
